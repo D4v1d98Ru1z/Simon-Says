d41de535365ec97af88f0d81b23fd3b8cacc20fc
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-11 14:17:46 -0500

    UPDATE: adding the new colors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ class Game{
     constructor(){
         this.init()
         this.generateSequence()
+        this.nextLevel()
     }
 
     init(){
@@ -23,7 +24,40 @@ class Game{
 
     generateSequence(){
         //the numer 10 in the array is going to represent the number of levels of the game
-        this.sequence = new Array (10).fill(0s).map(numRand => Math.floor(Math.random() * 4))
+        this.sequence = new Array (10).fill(0).map(numRand => Math.floor(Math.random() * 4))
+    }
+
+    nextLevel(){
+        this.iluminateSequence()
+    }
+
+    transNumtoColor(number){
+        switch (number){
+            case 0:
+                return 'blue'
+            case 1: 
+                return 'violet'
+            case 2:
+                return 'orange'
+            case 3:
+                return 'green'
+        }
+    }
+
+    iluminateSequence(){
+        for(let counter = 0; counter < this.level; counter++){
+            const color = this.transNumtoColor(this.sequence[counter])
+            setTimeout(() => this.iluminateColor(color), 1000 * counter)
+        }
+    }
+
+    iluminateColor(color){
+        this.colors[color].classList.add('light')
+        setTimeout(() => this.shutdownColor(color), 350)
+    }
+
+    shutdownColor(color){
+        this.colors[color].classList.remove('light')
     }
 }
 
